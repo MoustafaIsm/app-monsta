d672e61b7c8baef9b4a0977e9c49851515f4de13
Moustafa Ismail <moustafaism013@hotmail.com>
2023-01-20 13:47:42 +0200

    Frontend => Generated the required pages of the app

--- frontend/src/app/pages/home/app/app.module.ts
+++ frontend/src/app/pages/home/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
+    CommonModule,
     AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ]
 })
 export class AppModule { }
