73b580d1b3d81cc2bfbd348f1aceffeacfdabfe8
Moustafa Ismail <moustafaism013@hotmail.com>
2023-01-20 20:13:42 +0200

    Frontend => Added the values of the country picker and date picker to vars

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -9,4 +9,18 @@ import { faAndroid, faAppStoreIos } from '@fortawesome/free-brands-svg-icons';
 export class HomeComponent {
   faAndroid = faAndroid;
   faAppStoreIos = faAppStoreIos;
+  country: string = '';
+  date: string = new Date().toISOString().slice(0, 10);
+
+  constructor() { }
+
+  changeCity(event: any) {
+    this.country = event.target.value;
+  }
+
+  changeDate(event: any) {
+    this.date = event.target.value;
+    console.log(this.date);
+  }
+
 }
--- frontend/src/app/pages/home/home.module.ts
+++ frontend/src/app/pages/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -14,7 +15,8 @@ import { ComponentsModule } from '../../components/components.module';
     CommonModule,
     HomeRoutingModule,
     FontAwesomeModule,
-    ComponentsModule
+    ComponentsModule,
+    FormsModule,
   ]
 })
 export class HomeModule { }
