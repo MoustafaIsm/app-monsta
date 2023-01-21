07975ed2a76f5cd186ca7bc5949ad0cb9637af9c
Moustafa Ismail <moustafaism013@hotmail.com>
2023-01-21 15:37:59 +0200

    Frontend => Added a method for submitting a form

--- frontend/src/app/pages/auth/auth.component.ts
+++ frontend/src/app/pages/auth/auth.component.ts
@@ -7,10 +7,24 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+
   authForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)])
   });
+
   constructor() { }
+
+  onFormSubmit() {
+    // Check if form is valid
+    if (this.authForm.valid) {
+      // Get form values
+      const { email, password } = this.authForm.value;
+      console.log('Form values', email, password);
+    } else {
+      // Form is invalid
+      console.log('Form is invalid');
+    }
+  }
 
 }
