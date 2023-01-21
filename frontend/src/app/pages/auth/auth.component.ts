750cf1d100b647eeca0ede26bfb889a02e05239b
Moustafa Ismail <moustafaism013@hotmail.com>
2023-01-21 15:19:17 +0200

    Frontend => Added the login form

--- frontend/src/app/pages/auth/auth.component.ts
+++ frontend/src/app/pages/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  constructor() { }
 
 }
