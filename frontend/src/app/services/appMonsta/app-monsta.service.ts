f634093b449c0b7927188c3a6cdd88d802020f0f
Moustafa Ismail <moustafaism013@hotmail.com>
2023-01-21 17:54:30 +0200

    Frontend => Generated a app monsta service with a get genre api

---
+++ frontend/src/app/services/appMonsta/app-monsta.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'Basic ' + Buffer.from(`${environment.appMonsta.username} : ${environment.appMonsta.password}`).toString('base64')
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppMonstaService {
+
+  constructor(private http: HttpClient) { }
+
+  getGenres(store: string, country: string, date: string): Observable<any> {
+    return this.http.get(`https://api.appmonsta.com/v1/stores/${store}/apps/top/genres.json?country=${country}&date=${date}`, httpOptions);
+  }
+
+}
