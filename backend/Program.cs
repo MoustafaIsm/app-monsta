2da7e0ae3f46d350f91e412347fc1986d4b3cb5f
Moustafa Ismail <moustafaism013@hotmail.com>
2023-01-22 18:06:58 +0200

    Frontend => Enabled Cors iin the project

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,11 +6,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+    });
+}); 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors("CorsPolicy");
+}
+else
 {
     app.UseSwagger();
     app.UseSwaggerUI();
